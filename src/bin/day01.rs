6a9970260249e397e73bc327851a88661e1d4094
Emil Eriksson <asheidan@gmail.com>
2023-12-02 20:28:57 +0100

    Cleanup of day01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,7 +2,11 @@ use std::io::BufRead;
 
 fn solution_a(data: &[String]) -> i32 {
     data.iter()
-        .map(|line| line.chars().filter(|c| c.is_digit(10)).collect::<Vec<_>>())
+        .map(|line| {
+            line.chars()
+                .filter(|c| c.is_ascii_digit())
+                .collect::<Vec<_>>()
+        })
         .map(|numbervector: Vec<char>| {
             [numbervector.first(), numbervector.last()]
                 .into_iter()
@@ -20,62 +24,37 @@ fn solution_b(data: &[String]) -> u32 {
 
     data.iter()
         .map(|line| {
-            println!("> {}", line);
-            line
-        })
-        .map(|line| {
-            print!("< ");
-            //let mut mut_line= String::from(line);
             let line_len = line.len();
-            let result = (0..line_len)
+
+            (0..line_len)
                 .filter_map(|start| {
                     let chunk = &line[start..];
-                    // eprintln!("  c: {}", chunk);
 
                     let first_char = chunk.chars().next().unwrap();
 
-                    let result = match first_char.is_digit(10) {
+                    match first_char.is_ascii_digit() {
                         true => Some(first_char.to_digit(10).unwrap()),
-                        false => names
-                            .iter()
-                            .enumerate()
-                            .filter_map(|(index, name)| {
-                                if chunk.starts_with(name) {
-                                    Some(index as u32 + 1)
-                                } else {
-                                    None
-                                }
-                            })
-                            .next(),
-                    };
-
-                    print!(
-                        "{}",
-                        match result {
-                            None => ' ',
-                            Some(number) => char::from_u32(number + ('0' as u32)).unwrap(),
-                        }
-                    );
-
-                    result
+                        false => names.iter().enumerate().find_map(|(index, name)| {
+                            if chunk.starts_with(name) {
+                                Some(index as u32 + 1)
+                            } else {
+                                None
+                            }
+                        }),
+                    }
                 })
-                .collect::<Vec<u32>>();
-
-            println!();
-
-            result
+                .collect::<Vec<u32>>()
         })
         .map(|numbers| 10 * numbers.first().unwrap() + numbers.last().unwrap())
-        .map(|line| {
-            println!("= {}\n", line);
-            line
-        })
         .sum()
 }
 
 fn main() {
     let stdin = std::io::stdin();
-    let data: Vec<String> = stdin.lock().lines().map(|line| line.unwrap()).collect();
+    let data: Vec<String> = stdin.lock()
+        .lines()
+        .map(|line| line.unwrap())
+        .collect();
 
     println!("A: {}", solution_a(&data));
     println!("B: {}", solution_b(&data));
