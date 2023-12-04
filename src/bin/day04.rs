6cce8b7940a1c4a6c36a4b52260b42808c5c1975
Emil Eriksson <asheidan@gmail.com>
2023-12-04 09:15:13 +0100

    Working solution for day04

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,21 +1,80 @@
+use std::collections::HashSet;
 use std::io::BufRead;
 
-fn solution_a(_data: &str) -> u32 {
-    0
+/// https://adventofcode.com/2023/day/4
+
+struct Card {
+    winners: HashSet<u32>,
+    numbers: Vec<u32>,
 }
 
-fn solution_b(_data: &str) -> u32 {
-    0
+impl From<String> for Card {
+    fn from(value: String) -> Self {
+        let mut data_iter = value.split(" | ");
+
+        let winners: HashSet<u32> = data_iter.next().unwrap().split_whitespace().skip(2)
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
+
+        let numbers: Vec<u32> = data_iter.next().unwrap().split_whitespace()
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect();
+
+
+        Card { winners, numbers }
+    }
+}
+
+fn solution_a(data: &[Card]) -> u32 {
+    data.iter()
+        .map(|card| {
+            // Number of wins
+            let number_of_wins: u32 = card.numbers.iter()
+                .filter_map(|number| card.winners.contains(number).then_some(1))
+                .sum();
+
+            if 0 < number_of_wins {
+                1 << number_of_wins.saturating_sub(1)
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn solution_b(data: &[Card]) -> u32 {
+    let number_of_wins: Vec<u32> = data.iter()
+        .map(|card| {
+            // Number of wins
+            card.numbers.iter()
+                .filter_map(|number| card.winners.contains(number).then_some(1))
+                .sum()
+        })
+        .collect();
+
+    let mut cards: Vec<u32> = (0..number_of_wins.len()).map(|_| 1).collect();
+    assert_eq!(number_of_wins.len(), cards.len());
+
+    number_of_wins.iter().enumerate()
+        .for_each(|(index, &wins)| {
+            let current_count = *cards.get(index).unwrap();
+                  ((index + 1)..=(index+wins as usize)).for_each(|index| {
+                      //*cards.get_mut(index).unwrap() *= 2;
+                      *cards.get_mut(index).unwrap() += current_count;
+                  })
+        });
+
+
+    cards.iter().sum()
 }
 
 fn main() {
     let stdin = std::io::stdin();
-    let mut buffer = Vec::new();
-    stdin.lock()
-        //.lines()
-        .read_until(0, &mut buffer).unwrap();
-
-    let data  = String::from_utf8(buffer).unwrap();
+    let data: Vec<_> = stdin.lock()
+        .lines()
+        .map(|line| line.unwrap())
+        .map(Card::from)
+        .collect();
 
     println!("A: {}", solution_a(&data));
     println!("B: {}", solution_b(&data));
@@ -24,4 +83,22 @@ fn main() {
 #[cfg(test)]
 mod tests {
     //use super::*;
+
+    #[test]
+    fn sat_sub() {
+        // Given
+        let input = "1";
+
+        // When
+        let result = input.parse::<u32>().unwrap();
+
+        // Then
+        assert_eq!(1, result);
+    }
+
+    #[test]
+    fn empty_range() {
+        let result = (1..1).map(|_| 1).sum();
+        assert_eq!(0, result);
+    }
 }
